0d3a24a258904d5f9283d64a4c73f9289644e297
Sanzhar Taalaibek uulu <taalaibekuulusanzhar@gmail.com>
2023-05-14 15:19:46 +0600

    first commit

---
+++ src/main/java/com/selforderingmenu/SelfOrderingMenuApplication.java
@@ -0,0 +1,13 @@
+package com.selforderingmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfOrderingMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfOrderingMenuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/selforderingmenu/SelfOrderingMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selforderingmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfOrderingMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
